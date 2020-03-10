104f2210bd1ae2ccaf1d8ab9a74547860af78f63
Vaeliwen <zaman@imbriaco.com>
2020-03-10 13:47:04 -0400

    Set up basic mod development environment.

---
+++ Default-MDK/src/main/java/com/vaeliwen/banana/ModTutorialMod.java
@@ -0,0 +1,44 @@
+package com.vaeliwen.banana;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("banana")
+public class ModTutorialMod
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ModTutorialMod()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        //Registers you to the server and other game events you're interested in.
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event)
+    {
+
+    }
+
+}
